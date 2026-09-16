594e961548f1e397d580c43835eac6cfa3ca3b8a
Assaf emad <sofa@Assafs-MacBook-Air.local>
2026-09-16 04:22:16 +0300

    fix(index): oops forgot to configre the dotenv lib i hope i'll be forgiven for this

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express()
 const port =  process.env.PORT || 3000
 const crypto = require('crypto')
+require('dotenv').config()
+
 let HOST_URL = process.env.URL 
 const pendingAuth = new Map();
 
