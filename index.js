d8bf292dabd7e66fa9e382f2d98a6dc2826311b7
Assaf emad <sofa@Assafs-MacBook-Air.local>
2026-09-16 04:01:19 +0300

    feat(index.js): added a auth/callback route and handled the logic for the auth feat(index.js): added a code/token exchange route which exchanges code sent from the hackatime handler on the user's linux machine with a token and sends it back chore: i just made everything ready so i can deploy it

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+const axios = require('axios');
 const express = require('express');
 const app = express()
 const port =  process.env.PORT || 3000
+const crypto = require('crypto')
+let HOST_URL = process.env.URL 
+const pendingAuth = new Map();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/auth/callback',async (req, res) => {
+    let code = req.query.code
+    let state = req.query.state
+    let ACCESS_TOKEN_RES = await axios.post('https://hackatime.hackclub.com/oauth/token',
+        `client_id=Utwc3_2HeYUUZAYlRnaHQSqR7JhrHmygVjoumY_iqOY&code=${code}&grant_type=authorization_code&state${state}&redirect_uri=${HOST_URL}/auth/callback`,{
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+    let token = ACCESS_TOKEN_RES.data.access_token
+    let qscode = crypto.randomBytes(32).toString('hex')
+    pendingAuth.set(qscode, {
+        token: token,
+        expires: Date.now() + 60_000
+    });
+    res.redirect(`hackatime://auth/callback?code=${qscode}`)
+})
+app.post('/auth/exchange',async (req, res) => {
+    let code = req.query.code
+    const auth = pendingAuth.get(code);
+    if (!auth || auth.expires < Date.now()) {
+        return res.status(401).json({
+            error: 'Invalid or expired code'
+        });
+    }
+    pendingAuth.delete(code);
+    res.json({
+        access_token: auth.token
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
