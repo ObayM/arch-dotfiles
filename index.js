1c10eaf567193d29fa710fda5096130546971c58
Assaf emad <sofa@Assafs-MacBook-Air.local>
2026-09-17 20:34:34 +0300

    chore(index): added console logs to try to debug

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.get('/auth/callback',async (req, res) => {
 app.post('/auth/exchange',async (req, res) => {
     let code = req.body.code
     const auth = pendingAuth.get(code);
+    console.log(code)
+    console.log(pendingAuth)
+    console.log(auth)
     if (!auth || auth.expires < Date.now()) {
         return res.status(401).json({
             error: 'Invalid or expired code'
